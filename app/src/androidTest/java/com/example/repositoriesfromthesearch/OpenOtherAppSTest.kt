acc4099e49630ac48a4b03c9f1c1891f0f24d446
Stepan <bai_777@mail.ru>
2022-03-22 20:13:46 +0300

    реализовал классы BehaviorTest.kt, InitialTest.kt, OpenOtherAppSTest.kt

---
+++ app/src/androidTest/java/com/example/repositoriesfromthesearch/OpenOtherAppSTest.kt
@@ -0,0 +1,36 @@
+package com.example.repositoriesfromthesearch
+
+import android.widget.TextView
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SdkSuppress
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiScrollable
+import androidx.test.uiautomator.UiSelector
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SdkSuppress(minSdkVersion = 18)
+class OpenOtherAppSTest {
+
+    private val uiDevice: UiDevice = UiDevice.getInstance(getInstrumentation())
+
+    @Test
+    fun test_OpenSettings(){
+        uiDevice.pressHome()
+        uiDevice.swipe(500, 1500, 500, 0, 5)
+        val appViews = UiScrollable(UiSelector().scrollable(true))
+        // горизонтальная прокрутка (по умолчанию она вертикальная)
+        appViews.setAsHorizontalList()
+        val settingsApp = appViews.getChildByText(
+                UiSelector()
+                    .className(TextView::class.java.name), "Settings"
+            )
+        settingsApp.clickAndWaitForNewWindow()
+
+        val settingsValidation = uiDevice.findObject(UiSelector().packageName("com.android.settings"))
+        assertTrue(settingsValidation.exists())
+    }
+}
