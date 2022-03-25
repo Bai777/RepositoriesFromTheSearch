be4ffc986d21db301aaa7b523350107a370fc359
bai_777 <bai_777@mail.ru>
2022-03-25 20:38:26 +0300

    протестировал MainActivityTest.kt с помощью firebase

--- app/src/androidTest/java/view/search/MainActivityTest.kt
+++ app/src/androidTest/java/view/search/MainActivityTest.kt
@@ -7,15 +7,16 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
 import com.example.repositoriesfromthesearch.R
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
-import org.junit.*
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @LargeTest
