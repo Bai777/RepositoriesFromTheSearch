2fb625eb770225723b1e4d8a01b370798d8bcd86
bai_777 <bai_777@mail.ru>
2022-03-29 18:40:29 +0300

    начал дз 7

--- app/src/main/java/view/details/DetailsActivity.kt
+++ app/src/main/java/view/details/DetailsActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.repositoriesfromthesearch.R
 import com.example.repositoriesfromthesearch.databinding.DetailsActivityBinding
+import com.example.repositoriesfromthesearch.databinding.FragmentDetailsBinding
 import kotlinx.android.synthetic.main.details_activity.*
 import presenter.details.DetailsPresenter
 import presenter.details.IPresenterDetailsContract
@@ -14,11 +15,11 @@ import java.util.*
 class DetailsActivity : AppCompatActivity(), IViewDetailsContract {
 
     private val presenter: IPresenterDetailsContract = DetailsPresenter(this)
-    lateinit var binding: DetailsActivityBinding
+    lateinit var binding: FragmentDetailsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DetailsActivityBinding.inflate(layoutInflater)
+        binding = FragmentDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setUI()
     }
@@ -36,7 +37,7 @@ class DetailsActivity : AppCompatActivity(), IViewDetailsContract {
     }
 
     private fun setCountText(count: Int) {
-        totalCountTextViewDetails.text =
+        binding.totalCountTextViewDetails.text =
             String.format(Locale.getDefault(), getString(R.string.results_count), count)
     }
 
