d1fd4face90104196b07c3ad4537971a21dae111
bai_777 <bai_777@mail.ru>
2022-03-29 19:21:57 +0300

    добавил DetailsFragment.kt

--- app/src/main/java/view/details/DetailsActivity.kt
+++ app/src/main/java/view/details/DetailsActivity.kt
@@ -6,44 +6,26 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.repositoriesfromthesearch.R
 import com.example.repositoriesfromthesearch.databinding.DetailsActivityBinding
-import com.example.repositoriesfromthesearch.databinding.FragmentDetailsBinding
-import kotlinx.android.synthetic.main.details_activity.*
-import presenter.details.DetailsPresenter
-import presenter.details.IPresenterDetailsContract
-import java.util.*
 
-class DetailsActivity : AppCompatActivity(), IViewDetailsContract {
+class DetailsActivity : AppCompatActivity() {
 
-    private val presenter: IPresenterDetailsContract = DetailsPresenter(this)
-    lateinit var binding: FragmentDetailsBinding
+    lateinit var binding: DetailsActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FragmentDetailsBinding.inflate(layoutInflater)
+        binding = DetailsActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setUI()
-    }
-
-    private fun setUI() {
-        val count = intent.getIntExtra(TOTAL_COUNT_EXTRA, 0)
-        presenter.setCounter(count)
-        setCountText(count)
-        binding.decrementButton.setOnClickListener { presenter.onDecrement() }
-        binding.incrementButton.setOnClickListener { presenter.onIncrement() }
-    }
-
-    override fun setCount(count: Int) {
-        setCountText(count)
-    }
-
-    private fun setCountText(count: Int) {
-        binding.totalCountTextViewDetails.text =
-            String.format(Locale.getDefault(), getString(R.string.results_count), count)
+        supportFragmentManager.beginTransaction()
+            .add(
+                R.id.detailsFragmentContainer,
+                DetailsFragment.newInstance(intent.getIntExtra(TOTAL_COUNT_EXTRA, 0))
+            )
+            .commitAllowingStateLoss()
     }
 
     companion object {
 
-        const val TOTAL_COUNT_EXTRA = "TOTAL_COUNT_EXTRA"
+        private const val TOTAL_COUNT_EXTRA = "TOTAL_COUNT_EXTRA"
 
         fun getIntent(context: Context, totalCount: Int): Intent {
             return Intent(context, DetailsActivity::class.java).apply {
