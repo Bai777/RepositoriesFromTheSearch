184f22fafa852c62df331995c9e6077b73866bb3
bai_777 <bai_777@mail.ru>
2022-03-30 19:55:53 +0300

    закончил дз7

--- app/src/androidTest/java/com/example/repositoriesfromthesearch/MainActivityRecyclerViewTest.kt
+++ app/src/androidTest/java/com/example/repositoriesfromthesearch/MainActivityRecyclerViewTest.kt
@@ -1,11 +1,16 @@
 package com.example.repositoriesfromthesearch
 
+import android.view.View
+import android.widget.EditText
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -62,6 +67,42 @@ class MainActivityRecyclerViewTest {
         }
     }
 
+    @Test
+    fun activitySearch_PerformCustomClick(){
+        if (BuildConfig.TYPE == MainActivity.FAKE){
+            loadList()
+            onView(withId(R.id.recyclerView)).perform(
+                RecyclerViewActions.actionOnItemAtPosition<SearchResultAdapter.SearchResultViewHolder>(
+                    0, tapOnItemWithId(R.id.checkbox)
+                )
+            )
+        }
+    }
+
+    @Test
+    fun activityInsertTextFromEditText(){
+        if (BuildConfig.TYPE == MainActivity.FAKE){
+            loadList()
+            onView(withId(R.id.searchEditText)).perform(typeTextInChildViewWithId(0, "algol"))
+        }
+    }
+
+    private fun typeTextInChildViewWithId(id: Int, textToBeTyped: String): ViewAction {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View>? {
+                return null
+            }
+            override fun getDescription(): String {
+                return "Описание действия"
+            }
+            override fun perform(uiController: UiController, view: View) {
+                val v = view.findViewById<View>(id) as EditText
+                v.setText(textToBeTyped)
+            }
+        }
+    }
+
+
     private fun loadList() {
         onView(withId(R.id.searchEditText)).perform(click())
         onView(withId(R.id.searchEditText)).perform(
@@ -69,6 +110,21 @@ class MainActivityRecyclerViewTest {
             closeSoftKeyboard()
         )
         onView(withId(R.id.recyclerView)).perform(pressImeActionButton())
+    }
+
+    private fun tapOnItemWithId(id: Int) = object : ViewAction{
+        override fun getDescription(): String {
+            return "Нажимаем на view с указанным id"
+        }
+
+        override fun getConstraints(): Matcher<View>? {
+            return null
+        }
+
+        override fun perform(uiController: UiController?, view: View?) {
+            val clickCheckBox = view?.findViewById(id) as View
+            clickCheckBox.performClick()
+        }
     }
 
     @After
