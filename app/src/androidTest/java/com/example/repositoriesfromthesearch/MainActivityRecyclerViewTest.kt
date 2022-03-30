118a118db0d3103c82cf6d861ba9173370b806e7
bai_777 <bai_777@mail.ru>
2022-03-30 19:06:57 +0300

    протестировал RecyclerView в MainActivityRecyclerViewTest.kt

--- app/src/androidTest/java/com/example/repositoriesfromthesearch/MainActivityRecyclerViewTest.kt
+++ app/src/androidTest/java/com/example/repositoriesfromthesearch/MainActivityRecyclerViewTest.kt
@@ -1,11 +1,17 @@
 package com.example.repositoriesfromthesearch
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import view.search.MainActivity
+import view.search.SearchResultAdapter
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityRecyclerViewTest {
@@ -14,6 +20,55 @@ class MainActivityRecyclerViewTest {
     @Before
     fun setup(){
         scenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun activitySearch_ScrollTo(){
+        if (BuildConfig.TYPE == MainActivity.FAKE){
+            loadList()
+            onView(withId(R.id.recyclerView)).perform(RecyclerViewActions.scrollTo<
+                    SearchResultAdapter.SearchResultViewHolder>(
+                hasDescendant(withText("FullName: 42"))
+                    ))
+        }
+    }
+
+   @Test
+   fun activitySearch_PerformClickPosition(){
+       if (BuildConfig.TYPE == MainActivity.FAKE){
+           loadList()
+           onView(withId(R.id.recyclerView)).perform(
+               RecyclerViewActions.actionOnItemAtPosition<SearchResultAdapter.SearchResultViewHolder>(
+                   0, click()
+               )
+           )
+       }
+   }
+
+    @Test
+    fun activitySearch_PerformClickOnItem(){
+        if(BuildConfig.TYPE == MainActivity.FAKE){
+            loadList()
+            onView(withId(R.id.recyclerView)).perform(
+                RecyclerViewActions.scrollTo<SearchResultAdapter.SearchResultViewHolder>(
+                    hasDescendant(withText("FullName: 50"))
+                )
+            )
+            onView(withId(R.id.recyclerView)).perform(
+                RecyclerViewActions.actionOnItem<SearchResultAdapter.SearchResultViewHolder>(
+                    hasDescendant(withText("FullName: 42")), click()
+                )
+            )
+        }
+    }
+
+    private fun loadList() {
+        onView(withId(R.id.searchEditText)).perform(click())
+        onView(withId(R.id.searchEditText)).perform(
+            replaceText("algol"),
+            closeSoftKeyboard()
+        )
+        onView(withId(R.id.recyclerView)).perform(pressImeActionButton())
     }
 
     @After
