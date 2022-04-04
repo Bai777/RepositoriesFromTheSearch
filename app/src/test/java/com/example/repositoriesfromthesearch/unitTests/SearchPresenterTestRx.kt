bc144526b769d5956f28b1e16131b2ec394e80bb
bai_777 <bai_777@mail.ru>
2022-04-04 19:28:35 +0300

    протестировал SearchPresenter.kt

--- app/src/test/java/com/example/repositoriesfromthesearch/unitTests/SearchPresenterTestRx.kt
+++ app/src/test/java/com/example/repositoriesfromthesearch/unitTests/SearchPresenterTestRx.kt
@@ -1,14 +1,12 @@
 package com.example.repositoriesfromthesearch.unitTests
 
 import com.example.repositoriesfromthesearch.unitTests.stub.ScheduleProviderStub
-import com.nhaarman.mockito_kotlin.times
-import com.nhaarman.mockito_kotlin.verify
 import io.reactivex.Observable
 import model.SearchResponse
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
+import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
 import presenter.search.SearchPresenter
 import repository.GitHubRepository
@@ -24,14 +22,14 @@ class SearchPresenterTestRx {
     private lateinit var viewContract: IViewSearchContract
 
     @Before
-    fun setUp(){
+    fun setUp() {
         MockitoAnnotations.initMocks(this)
         presenter = SearchPresenter(repository, ScheduleProviderStub())
     }
 
     //Проверим вызов метода searchGitHub() у нашего Репозитория
     @Test
-    fun searchGitHub_Test(){
+    fun searchGitHub_Test() {
         `when`(repository.searchGithub(SEARCH_QUERY)).thenReturn(
             Observable.just(
                 SearchResponse(1, listOf())
@@ -41,9 +39,72 @@ class SearchPresenterTestRx {
         verify(repository, times(1)).searchGithub(SEARCH_QUERY)
     }
 
+    //Проверяем как обрабатывается ошибка запроса
+    @Test
+    fun handleRequestError_Test() {
+        `when`(repository.searchGithub(SEARCH_QUERY)).thenReturn(
+            Observable.error(
+                Throwable(
+                    ERROR_TEXT
+                )
+            )
+        )
+        presenter.searchGitHub(SEARCH_QUERY)
+        verify(viewContract, times(1)).displayError("error")
+    }
+
+    //Проверяем как обрабатываются неполные данные
+    @Test
+    fun handleResponseError_TotalCountIsNull() {
+        `when`(repository.searchGithub(SEARCH_QUERY)).thenReturn(
+            Observable.just(
+                SearchResponse(
+                    null,
+                    listOf()
+                )
+            )
+        )
+        presenter.searchGitHub(SEARCH_QUERY)
+        verify(viewContract, times(1)).displayError(
+            "Search results or total count are null"
+        )
+    }
+
+    //Проверим порядок вызова методов viewContract при ошибке
+    @Test
+    fun handleResponseError_TotalCountIsNull_ViewContractMethodOrder() {
+        `when`(repository.searchGithub(SEARCH_QUERY)).thenReturn(
+            Observable.just(
+                SearchResponse(
+                    null,
+                    listOf()
+                )
+            )
+        )
+        presenter.searchGitHub(SEARCH_QUERY)
+        val inOrder = inOrder(viewContract)
+        inOrder.verify(viewContract).displayLoading(true)
+        inOrder.verify(viewContract).displayError("Search results or total count are null")
+        inOrder.verify(viewContract).displayLoading(false)
+    }
+
+    //Проверим успешный ответ сервера
+    @Test
+    fun handleResponseSuccess(){
+        `when`(repository.searchGithub(SEARCH_QUERY)).thenReturn(
+            Observable.just(
+                SearchResponse(
+                    42,
+                    listOf()
+                )
+            )
+        )
+        presenter.searchGitHub(SEARCH_QUERY)
+        verify(viewContract, times(1)).displaySearchResults(listOf(), 42)
+    }
 
 
-    companion object{
+    companion object {
         private const val SEARCH_QUERY = "some query"
         private const val ERROR_TEXT = "error"
     }
