d1a68f3793dea1c663c8b23ef365206a1c100022
bai_777 <bai_777@mail.ru>
2022-04-05 18:40:07 +0300

    реализовал класс SearchViewModel.kt

--- app/src/main/java/presenter/search/SearchViewModel.kt
+++ app/src/main/java/presenter/search/SearchViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 import io.reactivex.observers.DisposableObserver
 import model.SearchResponse
 import presenter.IRepositoryContract
@@ -26,33 +25,42 @@ class SearchViewModel(
             .build().create(GitHubApi::class.java)
     ),
     private val appSchedulerProvider: SchedulerProvider = SearchSchedulerProvider()
-): ViewModel() {
+) : ViewModel() {
 
     private val _liveData = MutableLiveData<ScreenState>()
     private val liveData: LiveData<ScreenState> = _liveData
 
     fun subscribeToLiveData() = liveData
-    fun searchGitHub(searchQuery: String){
+    fun searchGitHub(searchQuery: String) {
         //Dispose
         val compositeDisposable = CompositeDisposable()
         compositeDisposable.add(
             repository.searchGithub(searchQuery)
                 .subscribeOn(appSchedulerProvider.io())
                 .observeOn(appSchedulerProvider.ui())
-                .doOnSubscribe{_liveData.value = ScreenState.Loading}
-                .subscribeWith(object: DisposableObserver<SearchResponse>()
-                {
-                    override fun onNext(t: SearchResponse) {
-                        TODO("Not yet implemented")
+                .doOnSubscribe { _liveData.value = ScreenState.Loading }
+                .subscribeWith(object : DisposableObserver<SearchResponse>() {
+                    override fun onNext(searchResponse: SearchResponse) {
+                        val searchResults = searchResponse.searchResults
+                        val totalCount = searchResponse.totalCount
+                        if (searchResults != null && totalCount != null) {
+                            _liveData.value = ScreenState.Working(searchResponse)
+                        } else {
+                            _liveData.value = ScreenState.Error(
+                                Throwable("Search results or total count are null")
+                            )
+                        }
                     }
 
                     override fun onError(e: Throwable) {
-                        TODO("Not yet implemented")
+                        _liveData.value = ScreenState.Error(
+                            Throwable(
+                                e.message ?: "Response is null or unsuccessful"
+                            )
+                        )
                     }
 
-                    override fun onComplete() {
-                        TODO("Not yet implemented")
-                    }
+                    override fun onComplete() {}
 
                 }
                 )
@@ -60,8 +68,8 @@ class SearchViewModel(
     }
 }
 
-sealed class ScreenState{
-    object Loading: ScreenState()
-    data class Working(val searchResponse: SearchResponse): ScreenState()
-    data class Error(val error: Throwable): ScreenState()
+sealed class ScreenState {
+    object Loading : ScreenState()
+    data class Working(val searchResponse: SearchResponse) : ScreenState()
+    data class Error(val error: Throwable) : ScreenState()
 }
